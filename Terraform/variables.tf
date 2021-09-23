bd6edc98a0ef63244f6d898e5fd7f301047d0368
EdFazli <edfazli92@gmail.com>
2021-09-24 06:59:07 +0800

    updating gitignore and variables

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,7 +1,24 @@
 #########################
 # variables declaration #
 #########################
+locals {
+  env = terraform.workspace
+}
 variable "region" {
     type = string
     description = "Which region you want to provision your resources?"
+}
+
+#Define IAM User Access Key
+variable "access_key" {
+  description = "The access_key that belongs to the IAM user"
+  type        = string
+  sensitive   = true
+}
+
+#Define IAM User Secret Key
+variable "secret_key" {
+  description = "The secret_key that belongs to the IAM user"
+  type        = string
+  sensitive   = true
 }
