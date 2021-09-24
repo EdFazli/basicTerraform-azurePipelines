7bef865ba2cb54ee44126c57515eb8b1136ba7ba
EdFazli <edfazli92@gmail.com>
2021-09-25 03:03:36 +0800

    edit key path in backend

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -5,7 +5,7 @@ terraform {
     required_version = ">=1.0"
     backend "s3" {
         bucket = "edfazli92-terraform-statefile"
-        key    = "edfazli92-terraform-statefile/"
+        key    = "tf/"
         region = "ap-southeast-1"
         encrypt = true
         dynamodb_table = "tfstatelocking"
