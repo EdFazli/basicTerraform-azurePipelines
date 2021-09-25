e895385a3ae74e2f6cff1a29934c0f21a01ceab6
EdFazli <edfazli92@gmail.com>
2021-09-25 11:40:23 +0800

    updating vpc in main

--- Terraform/modules/vpc/main.tf
+++ Terraform/modules/vpc/main.tf
@@ -5,45 +5,6 @@ terraform {
     required_version = ">=1.0"
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-resource "aws_subnet" "this" {
-  count                   = length(lookup(var.private_subnets, local.env))
-  cidr_block              = lookup(var.private_subnets, local.env)[count.index]
-  vpc_id                  = aws_vpc.this.id
-  map_public_ip_on_launch = "true"
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  tags = {
-    Name = "${local.env}-subnet-${count.index + 1}"
-  }
-}
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-  tags = {
-    "Name" = "${local.env}-main-igw"
-  }
-}
-
-resource "aws_route_table" "this" {
-  vpc_id = aws_vpc.this.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
-  tags = {
-    "Name" = "${local.env}-main-rt"
-  }
-}
-
-resource "aws_route_table_association" "public_rta" {
-  count          = length(aws_subnet.this)
-  subnet_id      = aws_subnet.this[count.index].id
-  route_table_id = aws_route_table.this.id
-
-}
-
-
 ####################
 # define resources #
 ####################
