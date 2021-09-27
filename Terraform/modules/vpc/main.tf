b79af781f9681bee2a06b6521509de2b7c97dbe7
EdFazli <edfazli92@gmail.com>
2021-09-28 00:00:15 +0800

    vpc public routes in main updated

--- Terraform/modules/vpc/main.tf
+++ Terraform/modules/vpc/main.tf
@@ -219,12 +219,12 @@ resource "aws_route_table" "public" {
       "Name" = format("%s-${var.public_subnet_suffix}", "${local.env}-VPC")
     },
     "${var.tags[local.env]}",
-    var.public_route_table_tags,
+    "${var.public_route_table_tags[local.env]}",
   )
 }
 
 resource "aws_route" "public_internet_gateway" {
-  count = "${var.create_vpc[local.env]}" && var.create_igw && length("${var.public_subnets[local.env]}") > 0 ? 1 : 0
+  count = "${var.create_vpc[local.env]}" && "${var.create_igw[local.env]}" && length("${var.public_subnets[local.env]}") > 0 ? 1 : 0
 
   route_table_id         = aws_route_table.public[0].id
   destination_cidr_block = "0.0.0.0/0"
@@ -236,7 +236,7 @@ resource "aws_route" "public_internet_gateway" {
 }
 
 resource "aws_route" "public_internet_gateway_ipv6" {
-  count = "${var.create_vpc[local.env]}" && var.create_igw && "${var.enable_ipv6[local.env]}" && length("${var.public_subnets[local.env]}") > 0 ? 1 : 0
+  count = "${var.create_vpc[local.env]}" && "${var.create_igw[local.env]}" && "${var.enable_ipv6[local.env]}" && length("${var.public_subnets[local.env]}") > 0 ? 1 : 0
 
   route_table_id              = aws_route_table.public[0].id
   destination_ipv6_cidr_block = "::/0"
