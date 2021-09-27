f97d0155a8388dc17bc7ec1d70bbf3673b191d78
EdFazli <edfazli92@gmail.com>
2021-09-27 23:06:46 +0800

    flow logs updated

--- Terraform/modules/vpc/vpc-flow-logs.tf
+++ Terraform/modules/vpc/vpc-flow-logs.tf
@@ -2,14 +2,16 @@
 # VPC flow-logs #
 ############################
 locals {
+
+  env = terraform.workspace
   # Only create flow log if user selected to create a VPC as well
-  enable_flow_log = var.create_vpc && var.enable_flow_log
+  enable_flow_log = "${var.create_vpc[local.env]}" && "${var.enable_flow_log[local.env]}"
 
-  create_flow_log_cloudwatch_iam_role  = local.enable_flow_log && var.flow_log_destination_type != "s3" && var.create_flow_log_cloudwatch_iam_role
-  create_flow_log_cloudwatch_log_group = local.enable_flow_log && var.flow_log_destination_type != "s3" && var.create_flow_log_cloudwatch_log_group
+  create_flow_log_cloudwatch_iam_role  = local.enable_flow_log && "${var.flow_log_destination_type[local.env]}" != "s3" && "${var.create_flow_log_cloudwatch_iam_role[local.env]}"
+  create_flow_log_cloudwatch_log_group = local.enable_flow_log && "${var.flow_log_destination_type[local.env]}" != "s3" && "${var.create_flow_log_cloudwatch_log_group[local.env]}"
 
-  flow_log_destination_arn = local.create_flow_log_cloudwatch_log_group ? aws_cloudwatch_log_group.flow_log[0].arn : var.flow_log_destination_arn
-  flow_log_iam_role_arn    = var.flow_log_destination_type != "s3" && local.create_flow_log_cloudwatch_iam_role ? aws_iam_role.vpc_flow_log_cloudwatch[0].arn : var.flow_log_cloudwatch_iam_role_arn
+  flow_log_destination_arn = local.create_flow_log_cloudwatch_log_group ? aws_cloudwatch_log_group.flow_log[0].arn : "${var.flow_log_destination_arn[local.env]}"
+  flow_log_iam_role_arn    = "${var.flow_log_destination_type[local.env]}" != "s3" && local.create_flow_log_cloudwatch_iam_role ? aws_iam_role.vpc_flow_log_cloudwatch[0].arn : "${var.flow_log_cloudwatch_iam_role_arn[local.env]}"
 }
 
 ################################################################################
@@ -19,15 +21,15 @@ locals {
 resource "aws_flow_log" "this" {
   count = local.enable_flow_log ? 1 : 0
 
-  log_destination_type     = var.flow_log_destination_type
+  log_destination_type     = "${var.flow_log_destination_type[local.env]}"
   log_destination          = local.flow_log_destination_arn
-  log_format               = var.flow_log_log_format
+  log_format               = "${var.flow_log_log_format[local.env]}"
   iam_role_arn             = local.flow_log_iam_role_arn
-  traffic_type             = var.flow_log_traffic_type
+  traffic_type             = "${var.flow_log_traffic_type[local.env]}"
   vpc_id                   = local.vpc_id
-  max_aggregation_interval = var.flow_log_max_aggregation_interval
+  max_aggregation_interval = "${var.flow_log_max_aggregation_interval[local.env]}"
 
-  tags = merge(var.tags, var.vpc_flow_log_tags)
+  tags = merge("${var.tags[local.env]}", "${var.vpc_flow_log_tags[local.env]}")
 }
 
 ################################################################################
@@ -37,11 +39,11 @@ resource "aws_flow_log" "this" {
 resource "aws_cloudwatch_log_group" "flow_log" {
   count = local.create_flow_log_cloudwatch_log_group ? 1 : 0
 
-  name              = "${var.flow_log_cloudwatch_log_group_name_prefix}${local.vpc_id}"
-  retention_in_days = var.flow_log_cloudwatch_log_group_retention_in_days
-  kms_key_id        = var.flow_log_cloudwatch_log_group_kms_key_id
+  name              = "${var.flow_log_cloudwatch_log_group_name_prefix[local.env]}${local.vpc_id}"
+  retention_in_days = "${var.flow_log_cloudwatch_log_group_retention_in_days[local.env]}"
+  kms_key_id        = "${var.flow_log_cloudwatch_log_group_kms_key_id[local.env]}"
 
-  tags = merge(var.tags, var.vpc_flow_log_tags)
+  tags = merge("${var.tags[local.env]}", "${var.vpc_flow_log_tags[local.env]}")
 }
 
 resource "aws_iam_role" "vpc_flow_log_cloudwatch" {
@@ -49,9 +51,9 @@ resource "aws_iam_role" "vpc_flow_log_cloudwatch" {
 
   name_prefix          = "vpc-flow-log-role-"
   assume_role_policy   = data.aws_iam_policy_document.flow_log_cloudwatch_assume_role[0].json
-  permissions_boundary = var.vpc_flow_log_permissions_boundary
+  permissions_boundary = "${var.vpc_flow_log_permissions_boundary[local.env]}"
 
-  tags = merge(var.tags, var.vpc_flow_log_tags)
+  tags = merge("${var.tags[local.env]}", "${var.vpc_flow_log_tags[local.env]}")
 }
 
 data "aws_iam_policy_document" "flow_log_cloudwatch_assume_role" {
