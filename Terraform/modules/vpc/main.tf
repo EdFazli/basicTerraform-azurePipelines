baf1236990fb3a0ba98e3ebeee92d2ae8f8bcfcc
EdFazli <edfazli92@gmail.com>
2021-09-28 00:42:18 +0800

    vpc public nacl in main updated

--- Terraform/modules/vpc/main.tf
+++ Terraform/modules/vpc/main.tf
@@ -420,20 +420,20 @@ resource "aws_network_acl_rule" "public_inbound" {
 }
 
 resource "aws_network_acl_rule" "public_outbound" {
-  count = var.create_vpc && var.public_dedicated_network_acl && length("${var.public_subnets[local.env]}") > 0 ? length(var.public_outbound_acl_rules) : 0
+  count = "${var.create_vpc[local.env]}" && "${var.public_dedicated_network_acl[local.env]}" && length("${var.public_subnets[local.env]}") > 0 ? length("${var.public_outbound_acl_rules[local.env]}") : 0
 
   network_acl_id = aws_network_acl.public[0].id
 
   egress          = true
-  rule_number     = var.public_outbound_acl_rules[count.index]["rule_number"]
-  rule_action     = var.public_outbound_acl_rules[count.index]["rule_action"]
-  from_port       = lookup(var.public_outbound_acl_rules[count.index], "from_port", null)
-  to_port         = lookup(var.public_outbound_acl_rules[count.index], "to_port", null)
-  icmp_code       = lookup(var.public_outbound_acl_rules[count.index], "icmp_code", null)
-  icmp_type       = lookup(var.public_outbound_acl_rules[count.index], "icmp_type", null)
-  protocol        = var.public_outbound_acl_rules[count.index]["protocol"]
-  cidr_block      = lookup(var.public_outbound_acl_rules[count.index], "cidr_block", null)
-  ipv6_cidr_block = lookup(var.public_outbound_acl_rules[count.index], "ipv6_cidr_block", null)
+  rule_number     = "${var.public_outbound_acl_rules[local.env]}"[count.index]["rule_number"]
+  rule_action     = "${var.public_outbound_acl_rules[local.env]}"[count.index]["rule_action"]
+  from_port       = lookup("${var.public_outbound_acl_rules[local.env]}"[count.index], "from_port", null)
+  to_port         = lookup("${var.public_outbound_acl_rules[local.env]}"[count.index], "to_port", null)
+  icmp_code       = lookup("${var.public_outbound_acl_rules[local.env]}"[count.index], "icmp_code", null)
+  icmp_type       = lookup("${var.public_outbound_acl_rules[local.env]}"[count.index], "icmp_type", null)
+  protocol        = "${var.public_outbound_acl_rules[local.env]}"[count.index]["protocol"]
+  cidr_block      = lookup("${var.public_outbound_acl_rules[local.env]}"[count.index], "cidr_block", null)
+  ipv6_cidr_block = lookup("${var.public_outbound_acl_rules[local.env]}"s[count.index], "ipv6_cidr_block", null)
 }
 
 ################################################################################
